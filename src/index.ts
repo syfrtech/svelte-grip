ba4257beb6f3e9dac185a446b932497fd5345103
Jonathan Stanley <jonathan@bristleconeweb.com>
2022-02-28 13:30:30 -0500

    fix: add .js to resolve error: "Cannot find module"

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 // tested and tree shakable
 
-export * from "./disclosure";
-export * from "./tooltip";
+export * from "./disclosure.js";
+export * from "./tooltip.js";
 
-export * from "./specAria";
-export * from "./specHtml";
-export * from "./utils";
+export * from "./specAria.js";
+export * from "./specHtml.js";
+export * from "./utils.js";
