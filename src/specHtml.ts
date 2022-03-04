6d7285c04edcb3059a67cd2752049c550f80d8e3
Jonathan Stanley <jonathan@bristleconeweb.com>
2022-03-03 23:07:33 -0500

    docs: add note about show/close for dialog

--- src/specHtml.ts
+++ src/specHtml.ts
@@ -3,16 +3,17 @@ import { get, writable } from "svelte/store";
 //https://github.com/romkor/svelte-portal/blob/master/src/Portal.svelte
 
 /**
- * `open` attribute (ex: `<dialog open...`) indicates whether an element's contents are visible. It is available on certain elements:
- * - [dialog]( https://developer.mozilla.org/en-US/docs/Web/HTML/Element/dialog#attributes)
- * - [details](https://developer.mozilla.org/en-US/docs/Web/HTML/Element/details#attributes)
+ * `open` attribute (ex: `<dialog open...`) indicates whether an element's contents are visible. It is only available on these elements:
+ * 1. [dialog](https://html.spec.whatwg.org/multipage/interactive-elements.html#attr-dialog-open)
+ * 2. [details](https://html.spec.whatwg.org/multipage/interactive-elements.html#attr-details-open)
  * 
- * [MDN list of all attributes](https://developer.mozilla.org/en-US/docs/Web/HTML/Attributes)
+ * see also: ["open" in the MDN list of all attributes](https://developer.mozilla.org/en-US/docs/Web/HTML/Attributes)
  * 
 ## Note
 This differs from CSS [visibility](https://developer.mozilla.org/en-US/docs/Web/CSS/visibility) and [display](https://developer.mozilla.org/en-US/docs/Web/CSS/display) which are intended for styling; not necessarily state.
 
-[Some typescript implementations ignore HTMLDialogElement](https://github.com/microsoft/TypeScript-DOM-lib-generator/pull/1259#issue-1121802821) as of February 2022. Instead, we just check if the passed element contains the "open" property.
+[Some typescript implementations don't include HTMLDialogElement](https://github.com/microsoft/TypeScript-DOM-lib-generator/pull/1259#issue-1121802821) as of February 2022. Instead, we just check if the passed element contains the "open" property.
+ * 
  */
 
 export const openAttributeAction = (
@@ -20,13 +21,41 @@ export const openAttributeAction = (
   store: IsOpenStore
 ) => {
   const update = (store: IsOpenStore) => {
-    if ("open" in node) {
-      node.open = get(store.isOpen$);
-    }
+    node.open = get(store.isOpen$);
   };
   update(store);
   return { update };
 };
+
+/**
+ * Spec suggests using native functions [`show`](https://html.spec.whatwg.org/multipage/interactive-elements.html#dom-dialog-show) and [`close`](https://html.spec.whatwg.org/multipage/interactive-elements.html#dom-dialog-close) to open/close a dialog.
+ *
+ * This avoids the following potential problems:
+ * if we simply remove the open attribute:
+ * 1. The close event will not be fired.
+ * 2. The close() method, and any user-agent provided cancelation interface, will no longer be able to close the dialog.
+ * 3. If the dialog was shown using its showModal() method, the Document will still be blocked.
+ *
+ * However these issues are largely moot with regard to Svelte.
+ *
+ * Disclosure sets the attribute directly using @see openAttributeAction however the order may not be correct.
+ *
+ * # @todo dialog.showModal()
+ * - Displays the dialog element and makes it the top-most modal dialog.
+ * - This method honors the autofocus attribute.
+ */
+// export const openAttributeDialogAction = (
+//   node: HTMLElement & { open?: boolean; show?: () => void; close?: () => void },
+//   store: IsOpenStore
+// ) => {
+//   const update = (store: IsOpenStore) => {
+//     let isOpen = get(store.isOpen$);
+//     isOpen && "show" in node && node.show();
+//     !isOpen && "close" in node && node.close();
+//   };
+//   update(store);
+//   return { update };
+// };
 
 /**
  * A record which indicates visibility
